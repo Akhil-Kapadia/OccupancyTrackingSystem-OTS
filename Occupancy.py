b90b832234a4ef8c3e80ba07815e09c0190c7e39
jacobhewgley <jhewg18@gmail.com>
2021-02-16 17:52:55 -0600

    Database connection, creation, and insertion done

--- Occupancy.py
+++ Occupancy.py
@@ -10,14 +10,20 @@ conn = psycopg2.connect(database = DB_NAME, user = DB_USER, password = DB_PASS, 
 print("Database connected successfully")
 
 cur = conn.cursor()
-cur.execute ("CREATE TABLE Entry_Prevention_System_Table(id INTEGER, date DATE, time TIME, current_occupancy INTEGER, entries INTEGER, exits INTEGER);")
-
+cur.execute ("CREATE TABLE Entry_Prevention_System_Table(id INTEGER PRIMARY KEY NOT NULL, Date DATE NOT NULL, Time TIME NOT NULL, CurrentOccupancy INTEGER NOT NULL, Entry BOOLEAN NOT NULL, People INTEGER NOT NULL);")
 conn.commit()
 print("Table created successfully")
 
-
-
-
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(1, '2/16/2021', '10:15', 2, 'true', 2);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(2, '2/16/2021', '12:30', 5, 'true', 3);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(3, '2/16/2021', '13:45', 3, 'false', 2);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(4, '2/16/2021', '15:15', 4, 'true', 1);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(5, '2/16/2021', '16:55', 2, 'false', 2);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(6, '2/16/2021', '18:30', 3, 'true', 1);")
+cur.execute("INSERT INTO Entry_Prevention_System_Table (id, Date, Time, CurrentOccupancy, Entry, People) VALUES(7, '2/16/2021', '19:15', 0, 'false', 3);")
+conn.commit()
+print("Data interted successfully")
+conn.close()
 
 
 
