3d7d79a63f304912a18c18c81438f7f309ad517a
BuildTools <akhil.kapadia@ttu.edu>
2021-02-23 21:43:56 -0600

    Fixed Error 500 for now

--- OTS/settings.py
+++ OTS/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'No Secrets Here!'
+SECRET_KEY = 'Whats my secret key!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
--- app/views.py
+++ app/views.py
@@ -8,16 +8,10 @@ from .forms import AddEntry
 def home(request):
     currentOccupancy = Occupancy.objects.latest('Time').getOccupancy()
     now = dt.datetime.now()
-    #Forms
-    manualAdd = AddEntry(request.POST or None, request.FILES or None)
-    
-    if manualAdd.is_valid():
-        manualAdd.save()
 
     return render(request, "home.html", context=
     {
         "currentOccupancy": currentOccupancy,
         "datetime" : now,
-        'manualAdd' : manualAdd,
         'OccupancyPercentage' : currentOccupancy/10000
     })
